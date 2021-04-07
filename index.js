ec85dd315b268b738157686b8e0bc954bfe7cafc
Charles Grant Nichols <grant@inkdots.com>
2021-04-07 16:23:17 -0500

    commit tree 5a7f90860b5dabf524e2313e98f8f7b066469f4c

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]; 
+
+selectingDrivers()
+
+function createFareMultiplier(multiply) {
+    return function (int) {
+        return multiply * int;
+    }
+}
+
+createFareMultiplier()
+
+function fareDoubler(int) {return int*2}
+
+function fareTripler(int) {return int*3}
+
+
+function selectDifferentDrivers(drivers, driversReturn) {
+    return driversReturn(drivers)
+}
